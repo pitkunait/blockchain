59da29346e669ec20cc1e6e0aa46b416c34ebf09
Vassili <vassili@tftak.eu>
2021-02-03 12:07:14 +0200

    d false

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.register_blueprint(transaction_api, url_prefix='/api/transaction')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
